fa87e2f0d0817dd2bdfbd1a710c7f1f889107cfe
Felipe Ferraz <felipeferraz1398@gmail.com>
2024-03-13 01:01:41 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Services
 {
@@ -26,28 +25,9 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller obj) 
         {   
-            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
-
-        public async Task<Seller> FindByIdAsync(int id)
-        {
-            return await _context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(obj => obj.Id == id);
-        }
-
-        public async Task RemoveAsync(int id)
-        {
-            try
-            {
-                var obj = await _context.Seller.FindAsync(id);
-                _context.Seller.Remove(obj);
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateException e)
-            {
-                throw new IntegrityException("Can't delete seller because he/she has sales");
-            }
-        }
+        
     }
 }
