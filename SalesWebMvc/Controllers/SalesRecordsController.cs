ba89e90a5e412dca59341eee5718b9b33c0beaa7
Felipe Ferraz <felipeferraz1398@gmail.com>
2024-03-13 15:01:05 -0300

    Preparing sales search navigation views

---
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
